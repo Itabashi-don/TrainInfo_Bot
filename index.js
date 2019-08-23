a4225a34043a7042053be6a09a9dbc32ac7c6605
Genbu Hase <hasegenbu@gmail.com>
2019-08-23 09:33:42 +0900

    node-cronを導入

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const { Masto } = require("masto");
+const cron = require("node-cron");
+
 const { INSTANCE, TOKEN } = process.env;
 
 (async () => {
 	const client = await Masto.login({
 		uri: INSTANCE,
 		accessToken: TOKEN
+	});
+
+	cron.schedule("* * * * *", () => {
+		console.log("Running a scheduled task...");
+
+		/*client.createStatus({
+			status: "This is tooted by scheduled task on TrainInfo_Bot.",
+			visibility: "unlisted"
+		});*/
 	});
 })();
