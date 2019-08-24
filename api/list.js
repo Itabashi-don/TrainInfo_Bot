585b2ceccfb327249ea1544d05b2d31a2fa83812
Genbu Hase <hasegenbu@gmail.com>
2019-08-24 11:59:36 +0900

    詳細情報の取得を実装

--- api/list.js
+++ api/list.js
@@ -11,21 +11,21 @@ const { returnData } = require("./../lib/EndPoint");
  * @param {NowResponse} res
  */
 module.exports = async (req, res) => {
-	const doc = await scrapeClient.fetch("http://www.jikokuhyo.co.jp/news/list");
+	const $doc = (await scrapeClient.fetch("http://www.jikokuhyo.co.jp/news/list")).$;
 
-	const { $ } = doc;
-	const table = $(".corner_block.top_pad");
+	const table = $doc(".corner_block.top_pad");
 
 	const date = table.children(".corner_block_header3").text().trim();
-	const operations = table.find(".corner_block_content > ul > li a").map((i, elem) => {
-		const operation = $(elem).contents();
+	const operations = table.find(".corner_block_content > ul > li a").map(async (i, elem) => {
+		const operation = $doc(elem).contents();
 
 		const time = operation.first().text().trim();
-		const railway = $(elem).children(".accent_color").text();
+		const name = $doc(elem).children(".accent_color").text();
 		const status = operation.last().text().trim();
+		const detail = (await $doc(elem).click()).$(".corner_block_content > ul > li .corner_block_row_detail_d").text();
 
-		return { railway, status, date: `${date} ${time}` };
+		return { name, status, date: `${date} ${time}`, detail };
 	}).toArray();
 
-	return returnData(res, null, operations);
+	return returnData(res, null, await Promise.all(operations));
 };
--- test/cheerio-httpcli.js
+++ test/cheerio-httpcli.js
@@ -1,21 +1,21 @@
 const scrapeClient = require("cheerio-httpcli");
 
 (async () => {
-	const doc = await scrapeClient.fetch("http://www.jikokuhyo.co.jp/news/list");
+	const $doc = (await scrapeClient.fetch("http://www.jikokuhyo.co.jp/news/list")).$;
 
-	const { $ } = doc;
-	const table = $(".corner_block.top_pad");
+	const table = $doc(".corner_block.top_pad");
 
 	const date = table.children(".corner_block_header3").text().trim();
-	const operations = table.find(".corner_block_content > ul > li a").map((i, elem) => {
-		const operation = $(elem).contents();
+	const operations = table.find(".corner_block_content > ul > li a").map(async (i, elem) => {
+		const operation = $doc(elem).contents();
 
 		const time = operation.first().text().trim();
-		const name = $(elem).children(".accent_color").text();
+		const name = $doc(elem).children(".accent_color").text();
 		const status = operation.last().text().trim();
+		const detail = (await $doc(elem).click()).$(".corner_block_content > ul > li .corner_block_row_detail_d").text();
 
-		return { name, status, date: `${date} ${time}` };
+		return { name, status, date: `${date} ${time}`, detail };
 	}).toArray();
 
-	console.log(operations);
+	console.log(await Promise.all(operations));
 })();
