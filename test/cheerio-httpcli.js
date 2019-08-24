f81169bfe2cbbc1d9ffca47a103558815a90a5a9
Genbu Hase <hasegenbu@gmail.com>
2019-08-24 13:05:04 +0900

    コードのミスを修正

--- test/cheerio-httpcli.js
+++ test/cheerio-httpcli.js
@@ -7,14 +7,15 @@ const scrapeClient = require("cheerio-httpcli");
 
 	const date = table.children(".corner_block_header3").text().trim();
 	const operations = table.find(".corner_block_content > ul > li a").map(async (i, elem) => {
-		const operation = $doc(elem).contents();
+		const info = $doc(elem).contents();
 
-		const time = operation.first().text().trim();
+		const time = info.first().text().trim();
 		const name = $doc(elem).children(".accent_color").text();
-		const status = operation.last().text().trim();
+		const status = info.last().text().trim();
 		const detail = (await $doc(elem).click()).$(".corner_block_content > ul > li .corner_block_row_detail_d").text().trim();
 
-		return { name, status, detail, createdAt: `${date} ${time}` };
+		const operation = { name, status, detail, createdAt: `${date} ${time}` };
+		return operation;
 	}).toArray();
 
 	console.log(await Promise.all(operations));
