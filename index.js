196e4bda1fa8dc6b83e2bebf539e5b8e36c98208
Genbu Hase <hasegenbu@gmail.com>
2019-08-29 13:49:33 +0900

    expressを導入

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+const express = require("express");
 const Mastodon = require("./lib/Mastodon");
 
 
 
 (async () => {
-	const client = await Mastodon.getMastoInstance();
-	
-	console.log("Now index.js is ready!");
+	// const client = await Mastodon.getMastoInstance();
+
+	const app = express();
+	app.get("/api/:apiName", (req, res) => require(`./api/${req.params.apiName}`)(req, res));
+	app.listen(() => console.log("Now index.js is ready!"));
 })();
