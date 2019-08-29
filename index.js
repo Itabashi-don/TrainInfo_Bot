0a3760519df60be0122127f70539861caed3c926
Genbu Hase <hasegenbu@gmail.com>
2019-08-29 14:29:49 +0900

    Try: ポート固定

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const Mastodon = require("./lib/Mastodon");
 	// const client = await Mastodon.getMastoInstance();
 
 	const app = express();
+	app.set("PORT", process.env.PORT || 5000);
 	app.get("/api/:apiName", (req, res) => require(`./api/${req.params.apiName}`)(req, res));
-	app.listen(() => console.log("Now index.js is ready!"));
+
+	app.listen(app.get("PORT"), () => console.log(`Now index.js is ready on port ${app.get("PORT")}!`));
 })();
