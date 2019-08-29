9794c01229585e0b0dbd9fb333b81db06289c5ac
Genbu Hase <hasegenbu@gmail.com>
2019-08-29 14:49:26 +0900

    数種の変更 * returnErrorがエラーを返さない問題の修正 * /にpublicディレクトリを割り当て * POSTリクエストにエラーを吐くように

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
+
 const Mastodon = require("./lib/Mastodon");
+const { returnError } = require("./lib/EndPoint");
 
 
 
@@ -8,7 +10,10 @@ const Mastodon = require("./lib/Mastodon");
 
 	const app = express();
 	app.set("PORT", process.env.PORT || 5000);
+
+	app.use("/", express.static("./public/"));
 	app.get("/api/:apiName", (req, res) => require(`./api/${req.params.apiName}`)(req, res));
+	app.post("/api/:apiName", (req, res) => returnError(res, 400, new Error("A POST-Request is not allowed.")));
 
 	app.listen(app.get("PORT"), () => console.log(`Now index.js is ready on port ${app.get("PORT")}!`));
 })();
