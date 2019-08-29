76bf22da41fea4c79bd0ad5072eea3b67061d341
Genbu Hase <hasegenbu@gmail.com>
2019-08-29 10:35:17 +0900

    ToDo: ページを跨いだ複数Operationの取得に関する修正

--- test/api_list.js
+++ test/api_list.js
@@ -3,25 +3,37 @@ require("./../lib/Typedef");
 
 
 
-(async (req, res) => {
-	const $doc = (await scrapeClient.fetch("http://www.jikokuhyo.co.jp/news/list")).$;
+const fetchOperations = async (pageId = 1) => {
+	const { $ } = await scrapeClient.fetch(`http://www.jikokuhyo.co.jp/news/list/page/${pageId}`);
 
-	const table = $doc(".corner_block.top_pad");
+	const table = $(".corner_block.top_pad");
 	const date = table.children(".corner_block_header3").text().trim();
+
+	/** @type {Object<string, number>} */
+	const fetchedCounter = {};
 
 	/** @type {Promise<Operation>[]} */
 	const operations = table.find(".corner_block_content > ul > li a").map(async (i, elem) => {
-		const info = $doc(elem).contents();
+		const detailUrl = $(elem).url();
+		const fetchedCount = (typeof fetchedCounter[detailUrl] !== "number") ? (fetchedCounter[detailUrl] = 0) : (++fetchedCounter[detailUrl]);
+
+		const info = $(elem).contents();
 
 		const time = info.first().text().trim();
-		const name = $doc(elem).children(".accent_color").text();
+		const name = $(elem).children(".accent_color").text();
 		const status = info.last().text().trim();
-		const detail = (await $doc(elem).click()).$(".corner_block_content > ul > li .corner_block_row_detail_d").text().trim();
+		const detail = (await $(elem).click()).$(`.corner_block_content > ul > li:has() .corner_block_row_detail_d`).eq(fetchedCount).text().trim();
 
 		/** @type {Operation} */
 		const operation = { name, status, detail, createdAt: `${date} ${time}` };
 		return operation;
-	}).toArray();
+	}).get();
 
-	console.log(await Promise.all(operations));
+	return await Promise.all(operations);
+};
+
+
+
+(async (req, res) => {
+	console.log(await fetchOperations(1));
 })();
